c3951ee543fd214d3316891810ed7f68b08f9232
akshay2739 <akshaypatel2739@gmail.com>
2020-04-16 23:42:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 //import './App.css'
-//import Home from './Home'
+import Home from './Home'
 //import Tutorial from './Todo//Tutorial'
 //import Count from './Todo/Count'
-import Life from './Todo/Lifecycle/Life'
+//import Life from './Todo/Lifecycle/Life'
 export default class App extends Component {
   render() {
     return (
-     <Life />
+     <Home />
     )
   }
 }
